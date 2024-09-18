67e75f9ca658fc256e23ab4b055380c1c2b7fe78
Ayushjodd <ayushkalonia2016@gmail.com>
2024-09-18 15:03:15 +0530

    added footer

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="container mx-auto p-4 text-center text-sm text-gray-500">
+        © 2023 MyBlog. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
