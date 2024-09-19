986aaaba564421ed04ab6dc33fd5c017c7a2f770
Ayushjodd <ayushkalonia2016@gmail.com>
2024-09-19 22:21:08 +0530

    footer added

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,11 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="border-t">
-      <div className="container mx-auto p-4 text-center text-sm text-gray-500">
-        © 2023 MyBlog. All rights reserved.
-      </div>
+    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+      <p className="text-xs text-gray-500">
+        © 2023 Acme Blogs Inc. All rights reserved.
+      </p>
+      <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        <Link className="text-xs hover:underline underline-offset-4" href="#">
+          Terms of Service
+        </Link>
+        <Link className="text-xs hover:underline underline-offset-4" href="#">
+          Privacy
+        </Link>
+      </nav>
     </footer>
   );
 };
