80f13673db307e2bbdede038aa82cabdc0130ccc
Ayushjodd <ayushkalonia2016@gmail.com>
2024-09-26 13:16:24 +0530

    api to get all the blogs

---
+++ app/api/blogs/getBlogs/index.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/lib/auth";
+import prisma from "@/db/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return NextResponse.json(
+        {
+          message: "Please login to access this content",
+        },
+        { status: 401 }
+      );
+    }
+
+    const blogs = await prisma.blog.findMany({
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        content: true,
+        author: true,
+        createdAt: true,
+      },
+    });
+
+    return NextResponse.json({ blogs }, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: "An error occurred while fetching the blogs",
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
