dc1e57ed4f90591ff1c438176d9a789eacb2c7bf
Ayushjodd <ayushkalonia2016@gmail.com>
2024-09-28 22:45:08 +0530

    added new api

---
+++ app/api/blogs/[id]/route.ts
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+"use server";
+
+import { authOptions } from "@/app/lib/auth";
+import prisma from "@/db/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  try {
+    //@ts-ignore
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Unathorized",
+        },
+        { status: 401 }
+      );
+    }
+
+    const { id } = context.params;
+    const blogId = Number(id);
+
+    const blog = await prisma.blog.findUnique({
+      where: {
+        id: blogId,
+      },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        author: true,
+        content: true,
+        authorId: true,
+        createdAt: true,
+        likes: true,
+        likedBy: true,
+        category: true,
+      },
+    });
+
+    if (!blog) {
+      return NextResponse.json(
+        { success: false, message: "blog not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({ blog }, { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
