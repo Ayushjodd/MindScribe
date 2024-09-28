3aa935cf5f61cd34e47f61913ec657805f1a3de1
Ayushjodd <ayushkalonia2016@gmail.com>
2024-09-28 23:06:21 +0530

    quick fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { authOptions } from "@/app/lib/auth";
 import NextAuth from "next-auth/next";
 
 export const maxDuration = 60;
+//@ts-ignore
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
