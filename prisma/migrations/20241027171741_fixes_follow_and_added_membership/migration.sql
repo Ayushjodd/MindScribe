a369c07bc1d877093a04a40367d59f7d13cc1d9e
ayushjodd <ayushkalonia2016@gmail.com>
2024-10-27 22:48:54 +0530

    membership added

---
+++ prisma/migrations/20241027171741_fixes_follow_and_added_membership/migration.sql
@@ -0,0 +1,25 @@
+-- CreateEnum
+CREATE TYPE "MembershipType" AS ENUM ('BASIC', 'ADVANCE', 'PRO');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- CreateTable
+CREATE TABLE "Membership" (
+    "id" TEXT NOT NULL,
+    "type" "MembershipType" NOT NULL DEFAULT 'BASIC',
+    "userId" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "endDate" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Membership_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Membership_userId_key" ON "Membership"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Membership" ADD CONSTRAINT "Membership_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
